42db5eda3d51365b52cf717f198ff16bf68bb38a
aavram <aavram27@gmail.com>
2018-02-11 17:39:08 -0800

    loading catalogue page from controller

---
+++ application/views/category.php
@@ -0,0 +1,63 @@
+<h1>Catalog</h1>
+
+<div id="body">
+    <p>List of all accessories available</p>
+        
+    <table style="width:100%">
+        <tr>
+            <th></th>
+            <th align="left">Accessory Id</th> 
+            <th align="left">Description</th>
+            <th align="left">Cost</th>
+            <th align="left">Popularity</th>
+            <th align="left">Quality</th>
+        </tr>
+        {bodyColours}
+        <tr>
+            <td><img src="{Path}" style="width:200px"></td>
+            <td>{AccessoryId}</td> 
+            <td>{Description}</td>
+            <td>{Cost}</td>
+            <td>{Popularity}</td>
+            <td>{Quality}</td>
+        </tr>
+        {/bodyColours}
+        
+        {rims}
+        <tr>
+            <td><img src="{Path}" style="width:200px"></td>
+            <td>{AccessoryId}</td> 
+            <td>{Description}</td>
+            <td>{Cost}</td>
+            <td>{Popularity}</td>
+            <td>{Quality}</td>
+        </tr>
+        {/rims}
+        
+        {spoilers}
+        <tr>
+            <td><img src="{Path}" style="width:200px"></td>
+            <td>{AccessoryId}</td> 
+            <td>{Description}</td>
+            <td>{Cost}</td>
+            <td>{Popularity}</td>
+            <td>{Quality}</td>
+        </tr>
+        {/spoilers}
+        
+        {storages}
+        <tr>
+            <td><img src="{Path}" style="width:200px"></td>
+            <td>{AccessoryId}</td> 
+            <td>{Description}</td>
+            <td>{Cost}</td>
+            <td>{Popularity}</td>
+            <td>{Quality}</td>
+        </tr>
+        {/storages}
+        
+    </table>
+    
+        
+    
+</div>
