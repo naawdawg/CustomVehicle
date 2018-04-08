f7af2997fd48742ac289d107a0e282c10d6e39d0
MrOStanMan <lucaskondrackyj@gmail.com>
2018-04-07 18:48:15 -0700

    Switched Id sides

--- application/views/category.php
+++ application/views/category.php
@@ -8,54 +8,57 @@
         
     <table style="width:100%">
         <tr>
-            <th></th>
             <th align="left">Accessory Id</th> 
             <th align="left">Description</th>
             <th align="left">Cost</th>
             <th align="left">Popularity</th>
             <th align="left">Quality</th>
+            <th></th>
         </tr>
         {bodyColours}
         <tr>
-            <td><img src="{Path}" style="width:200px"></td>
+            
             <td>{AccessoryId}</td> 
             <td>{Description}</td>
             <td>{Cost}</td>
             <td>{Popularity}</td>
             <td>{Quality}</td>
+            <td><img src="{Path}" style="width:200px"></td>
+            
         </tr>
         {/bodyColours}
         
         {rims}
         <tr>
-            <td><img src="{Path}" style="width:200px"></td>
             <td>{AccessoryId}</td> 
             <td>{Description}</td>
             <td>{Cost}</td>
             <td>{Popularity}</td>
             <td>{Quality}</td>
+            <td><img src="{Path}" style="width:200px"></td>
+            
         </tr>
         {/rims}
         
         {spoilers}
         <tr>
-            <td><img src="{Path}" style="width:200px"></td>
             <td>{AccessoryId}</td> 
             <td>{Description}</td>
             <td>{Cost}</td>
             <td>{Popularity}</td>
             <td>{Quality}</td>
+            <td><img src="{Path}" style="width:200px"></td>
         </tr>
         {/spoilers}
         
         {storages}
         <tr>
-            <td><img src="{Path}" style="width:200px"></td>
             <td>{AccessoryId}</td> 
             <td>{Description}</td>
             <td>{Cost}</td>
             <td>{Popularity}</td>
             <td>{Quality}</td>
+            <td><img src="{Path}" style="width:200px"></td>
         </tr>
         {/storages}
         
